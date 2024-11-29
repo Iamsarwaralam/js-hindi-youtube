f4d6ac2375889c361e1067da82331284cf31d81d
Iamsarwaralam <sarwaralam409@gmail.com>
2024-11-29 15:14:30 +0000

    datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,6 +27,52 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+//Operations//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " sarwar"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + 2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + 2);
+console.log("1" + 2 + 2);
+console.log( 1 + 2 + "2");
+console.log(1 + "2" + 2);
+
+
+console.log("2" > 1);
+console.log("02" > 0);
+console.log("2" > 0);
+
+console.log(null > 0);
+console.log(null == 0);
+//console.log(null = 0);//
+console.log(null < 0);
+console.log(null >= 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
